d1b0438757ce057f433dd2f5d4cb3a0700351340
wakeless <wakeless@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-24 08:57:08 +0000

    Complete rewrite. haha
    git-svn-id: https://plugins.svn.wordpress.org/sexy-bookmarks-sidebar-plugin/trunk@221206 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sexy-bookmarks-sidebar-plugin.php
+++ sexy-bookmarks-sidebar-plugin.php
@@ -8,4 +8,31 @@ Version: 0.1
 Author URI: http://terroir.me/
 */
 
-register_sidebar_widget("Sexy Bookmarks", "selfserv_sexy"); 
+
+class Sexy_Bookmarks_Sidebar_Widget extends WP_Widget {
+	function Sexy_Bookmarks_Sidebar_Widget() {
+    	parent::WP_Widget(false, $name = "Sexy Bookmarks");
+	}
+
+	function widget($args, $instance) {		
+    	extract( $args );
+    	$title = apply_filters('widget_title', $instance['title']);
+        ?>
+        	<?php echo $before_widget; ?>
+            <?php if ( $title )
+            	echo $before_title . $title . $after_title; ?>
+            	selfserv_sexy();
+            <?php echo $after_widget; ?>
+        <?php
+    }
+    
+    function form($instance) {				
+        $title = esc_attr($instance['title']);
+        ?>
+            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+        <?php 
+    }
+
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("Sexy_Bookmarks_Sidebar_Widget");'));
